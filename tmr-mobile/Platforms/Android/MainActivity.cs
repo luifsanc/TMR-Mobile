9a3c741fb64502c70a8612698fa5cf0605cda15c
Adite16 <aldodimattia72@gmail.com>
2026-08-20 12:13:55 -0500

    cambios en modulo de lideres, alertas, icono y nombre de la app

--- tmr-mobile/Platforms/Android/MainActivity.cs
+++ tmr-mobile/Platforms/Android/MainActivity.cs
@@ -1,11 +1,11 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
 
 namespace tmr_mobile;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, Icon = "@mipmap/appicon", RoundIcon = "@mipmap/appicon_round", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 	protected override void OnCreate(Bundle? savedInstanceState)
