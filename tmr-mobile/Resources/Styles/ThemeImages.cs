f3f9051f9688171d2b909efeddecbd4b180f6977
Christopher Vera <Christopher.Vera@integritysolutions.com.ec>
2026-08-21 10:26:50 -0500

    mejora de margenes y de botones

--- tmr-mobile/Resources/Styles/ThemeImages.cs
+++ tmr-mobile/Resources/Styles/ThemeImages.cs
@@ -8,7 +8,7 @@ public static class ThemeImages
     private static readonly HashSet<string> IconsWithDarkVariant = new(StringComparer.OrdinalIgnoreCase)
     {
         "catalogo.png", "configura.png", "direccion.png", "feriado.png", "homes.png",
-        "icon_carga.png", "icon_clientes.png", "icon_home.png", "icon_lideres.png",
+        "icon_carga.png", "icon_clientes.png", "icon_colaboradores.png", "icon_home.png", "icon_lideres.png",
         "icon_proyectos.png", "icon_pulenes.png", "icon_reportes.png", "icon_seguimiento.png",
         "icon_timereport.png", "key_icon.png", "logout_icon.png",
         "notification_bell_alarm.png", "rol.png", "user_profile.png"
